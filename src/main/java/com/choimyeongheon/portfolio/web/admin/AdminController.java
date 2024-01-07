1a742e7ebb28c3f0517fbef8fec4496ef3a08a58
csh <tmdgjs7495@naver.com>
2024-01-08 00:14:22 +0900

    [feat] : homeImage th:each 처리 구현

--- src/main/java/com/choimyeongheon/portfolio/web/admin/AdminController.java
+++ src/main/java/com/choimyeongheon/portfolio/web/admin/AdminController.java
@@ -1,5 +1,6 @@
 package com.choimyeongheon.portfolio.web.admin;
 
+import com.choimyeongheon.portfolio.domain.home.domain.HomeImage;
 import com.choimyeongheon.portfolio.domain.home.service.HomeImageService;
 import com.choimyeongheon.portfolio.web.home.dto.HomeImageRequest;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
@@ -16,7 +19,11 @@ public class AdminController {
     private final HomeImageService homeImageService;
 
     @GetMapping("/admin/home")
-    public String admin() {
+    public String admin(Model model) {
+
+        List<HomeImage> homeImages = homeImageService.findAll();
+        model.addAttribute("homeImages", homeImages);
+
         return "admin/home";
     }
 
@@ -34,9 +41,10 @@ public class AdminController {
     @PostMapping("/admin/home/save")
     public String save(@ModelAttribute("request") HomeImageRequest request) {
 
+        // homeImageResponse 객체 생성 후 변경 - Entity 를 반환하지 않기
         homeImageService.createHomeImage(request);
 
-        return "home";
+        return "redirect:/admin/home";
     }
 
 }

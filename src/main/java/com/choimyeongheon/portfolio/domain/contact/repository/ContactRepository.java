7ad7083961d71ba6722d77a403842640ab393439
csh <tmdgjs7495@naver.com>
2024-04-15 23:17:33 +0900

    [feat] : 전체 Contact 조회 쿼리 변경 및 UI 단으로 데이터 전달

--- src/main/java/com/choimyeongheon/portfolio/domain/contact/repository/ContactRepository.java
+++ src/main/java/com/choimyeongheon/portfolio/domain/contact/repository/ContactRepository.java
@@ -2,6 +2,15 @@ package com.choimyeongheon.portfolio.domain.contact.repository;
 
 import com.choimyeongheon.portfolio.domain.contact.domain.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    @Query("SELECT COUNT(c.id) FROM Contact c WHERE c.readYn = 'N'")
+    Long countAllNotRead();
+
+    List<Contact> findAllByOrderByCreatedAtDesc();
+
 }

d8bfe9473596d8de927a48a4c8fe4ae77daa83a3
csh <tmdgjs7495@naver.com>
2024-10-04 17:51:12 +0900

    [feat] : work modal UI

--- src/main/java/com/choimyeongheon/portfolio/web/visitor/work/dto/VisitorWorkResponse.java
+++ src/main/java/com/choimyeongheon/portfolio/web/visitor/work/dto/VisitorWorkResponse.java
@@ -10,11 +10,13 @@ import lombok.Setter;
 @Setter
 public class VisitorWorkResponse {
 
+    private Long id;
     private String fileName;
     private String title;
     private String year;
 
     public VisitorWorkResponse(Work work) {
+        this.id = work.getId();
         this.fileName = work.getFileName();
         this.title = work.getTitle();
         this.year = String.valueOf(work.getWorkDate().getYear());
